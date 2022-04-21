e7d3336b4c9b08c56c54fcd528ae15f5e1ad4641
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-21 16:45:34 +0530

    first name added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,9 +4,7 @@ package com.bridgelabz;
 
 public class UserRegistration {
     public boolean checkFName(String fName) {
-        /**
-         * regex pattern for FirstName
-         */
+
         return (fName.matches("[A-Z][a-z]{3,}"));
     }
 }
